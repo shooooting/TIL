40a712fda2eea92a4022a2b8cba077dd31f79419
shooooting <shooootingsta@gmail.com>
2020-04-17 20:06:57 +0900

    UP

--- Swift-Q/Swift-Q.playground/Contents.swift
+++ Swift-Q/Swift-Q.playground/Contents.swift
@@ -163,15 +163,16 @@ func soNumber(num: Int) {
 
 soNumber(num: 10)
 
-// 옵셔널 타입의 문자열 파라미터 3개를 입력받은 뒤, 옵셔널을 추출하여 Unwrapped 된 하나의 문자열로 합쳐서 반환하는 함수
-//
-//func combineString(str1: String?, str2: String?, str3: String?) -> String {
-//  // code
-//}
-//
-//// 입력 예시 및 결과
-//combineString1(str1: "AB", str2: "CD", str3: "EF")   // "ABCDEF"
-//combineString1(str1: "AB", str2: nil, str3: "EF")    // "ABEF"
+/* 옵셔널 타입의 문자열 파라미터 3개를 입력받은 뒤, 옵셔널을 추출하여 Unwrapped 된 하나의 문자열로 합쳐서 반환하는 함수
+
+func combineString(str1: String?, str2: String?, str3: String?) -> String {
+  // code
+}
+
+// 입력 예시 및 결과
+combineString1(str1: "AB", str2: "CD", str3: "EF")   // "ABCDEF"
+combineString1(str1: "AB", str2: nil, str3: "EF")    // "ABEF"
+*/
 
 func combineString(str1: String?, str2: String?, str3: String?) -> String {
     let show1 = str1 ?? ""
@@ -186,34 +187,34 @@ func combineString(str1: String?, str2: String?, str3: String?) -> String {
 //combineString(str1: "AB", str2: "CD", str3: "EF")   // "ABCDEF"
 combineString(str1: "AB", str2: nil, str3: "EF")    // "ABEF"
 
-/*: 2. 사칙연산(+, -, *, /)을 가진 enum 타입 Arithmetic과 2개의 자연수를 입력 파라미터로 받아 (파라미터 총 3개) 해당 연산의 결과를 반환하는 함수 구현
-enum Arithmetic {
-  case addition, subtraction, multiplication, division
-}*/
+    /*: 2. 사칙연산(+, -, *, /)을 가진 enum 타입 Arithmetic과 2개의 자연수를 입력 파라미터로 받아 (파라미터 총 3개) 해당 연산의 결과를 반환하는 함수 구현
+    enum Arithmetic {
+      case addition, subtraction, multiplication, division
+    }*/
 
-enum Arithmetic {
-    case addition, subtraction, multiplication, division
-    
-    func soNumber(num1: Int, num2: Int) -> Double {
-        var answer: Double = 0.0
-        switch self {
-        case .addition:
-            answer = Double(num1 + num2)
-        case .subtraction:
-            answer = Double(num1 - num2)
-        case .multiplication:
-            answer = Double(num1 * num2)
-        case .division:
-            answer = Double(num1 / num2)
-        }
-        return answer
-    }
-}
+//enum Arithmetic {
+//    case addition, subtraction, multiplication, division
+//
+//    func soNumber(num1: Int, num2: Int) -> Double {
+//        var answer: Double = 0.0
+//        switch self {
+//        case .addition:
+//            answer = Double(num1 + num2)
+//        case .subtraction:
+//            answer = Double(num1 - num2)
+//        case .multiplication:
+//            answer = Double(num1 * num2)
+//        case .division:
+//            answer = Double(num1 / num2)
+//        }
+//        return answer
+//    }
+//}
+//
+//let a = Arithmetic.addition
+//a.soNumber(num1: 2, num2: 4)
 
-let a = Arithmetic.addition
-a.soNumber(num1: 2, num2: 4)
-
-//[ 도전 과제 ]
+/*[ 도전 과제 ]
 //1. celcius, fahrenheit, kelvin 온도 3가지 케이스를 가진 enum 타입 Temperature 를 정의
 //각 케이스에는 Double 타입의 Associated Value 를 받도록 함
 //
@@ -223,7 +224,7 @@ a.soNumber(num1: 2, num2: 4)
 //
 //enum Temperature {
 //  // 코드
-//}
+}*/
 
 enum Temperature {
     case celcius(Double), fahrenheit(Double), kelvin(Double)
@@ -243,3 +244,99 @@ enum Temperature {
 let temper = Temperature.fahrenheit(100.2)
 temper.toCelcius()
 
+func combineString1(str1: String?, str2: String?, str3: String?) -> String {
+    let numbering1 = str1 ?? ""
+    let numbering2 = str2 ?? ""
+    let numbering3 = str3 ?? ""
+    
+    let sumSummary = numbering1 + numbering2 + numbering3
+    return sumSummary
+    
+}
+
+// 입력 예시 및 결과
+combineString1(str1: "AB", str2: "CD", str3: "EF")   // "ABCDEF"
+//combineString1(str1: "AB", str2: nil, str3: "EF")    // "ABEF"
+
+
+/*: 2. 사칙연산(+, -, *, /)을 가진 enum 타입 Arithmetic과 2개의 자연수를 입력 파라미터로 받아 (파라미터 총 3개) 해당 연산의 결과를 반환하는 함수 구현
+enum Arithmetic {
+  case addition, subtraction, multiplication, division
+}*/
+
+enum Rithmetic {
+    case addition, subtraction, multiplication, division
+    
+    func Number(no1: Int, no2: Int) -> Double {
+        var sumthing: Double = 0.0
+        switch self {
+        case .addition:
+            sumthing = Double(no1 + no2)
+        case .subtraction:
+            sumthing = Double(no1 - no2)
+        case .multiplication:
+            sumthing = Double(no1 * no2)
+        case .division:
+            sumthing = Double(no1 / no2)
+        }
+        return sumthing
+    }
+}
+
+let sum = Rithmetic.addition
+sum.Number(no1: 5, no2: 2)
+
+/*[ 도전 과제 ]
+//1. celcius, fahrenheit, kelvin 온도 3가지 케이스를 가진 enum 타입 Temperature 를 정의
+//각 케이스에는 Double 타입의 Associated Value 를 받도록 함
+//
+//추가로 Temperature 타입 내부에 각 온도를 섭씨 온도로 변환해주는 toCelcius() 함수를 구현
+//섭씨 = (화씨 - 32) * 5 / 9
+//섭씨 = 켈빈 + 273
+//
+//enum Temperature {
+//  // 코드
+}*/
+
+enum Temperature1 {
+    case celcius(Double), fahrenheit(Double), kelvin(Double)
+    
+    func toCelcius() -> Double {
+        switch self {
+        case let .celcius(x):
+            return(x)
+        case let .fahrenheit(x):
+            return (x - 32) * 5 / 9
+        case let .kelvin(x):
+            return x + 273
+
+        }
+    }
+    
+}
+
+
+let so = Temperature1.fahrenheit(99.2)
+so.toCelcius()
+
+enum Ithmetic {
+    case addition, subtraction, multiplication, division
+
+    func math(num1: Int, num2: Int) -> Double {
+        var a: Double = 0.0
+        switch self {
+        case .addition:
+             a = Double(num1 + num2)
+        case .subtraction:
+             a = Double(num1 - num2)
+        case .multiplication:
+             a = Double(num1 * num2)
+        case .division:
+             a = Double(num1 / num2)
+        }
+        return a
+    }
+}
+
+let som = Ithmetic.subtraction
+som.math(num1: 2, num2: 4)
