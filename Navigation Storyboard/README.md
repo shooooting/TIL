2dddafa2e1e76f76c32455961db1adaa1d6542c5
shooooting <shooootingsta@gmail.com>
2020-05-10 23:30:29 +0900

    Navigation Controller Storyboard

---
+++ Navigation Storyboard/README.md
@@ -0,0 +1,4 @@
+# Navigation Controller 
+## Story board
+
+[Navigation Controller Storyboard](https://www.notion.so/shooooting/Navigation-Controller-04909a628f4040c3973f92c4640badcf)
---
+++ Navigation Storyboard/plactice/plactice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plactice
+//
+//  Created by ㅇ오ㅇ on 2020/05/10.
+//  Copyright © 2020 shooooting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
