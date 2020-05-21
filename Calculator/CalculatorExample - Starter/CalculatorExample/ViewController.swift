e62885f366242afcfe45aafd6872169bcb644550
shooooting <shooootingsta@gmail.com>
2020-05-21 23:48:41 +0900

    last formatter...

--- Calculator/CalculatorExample - Starter/CalculatorExample/ViewController.swift
+++ Calculator/CalculatorExample - Starter/CalculatorExample/ViewController.swift
@@ -1,13 +1,4 @@
-//
-//  ViewController.swift
-//  CalculatorExample
-//
-//  Created by giftbot on 2019/12/19.
-//  Copyright © 2019 giftbot. All rights reserved.
-//
-
 import UIKit
-
 final class ViewController: UIViewController {
     
     @IBOutlet weak var numberLabel: UILabel!
@@ -30,18 +21,16 @@ final class ViewController: UIViewController {
     @IBOutlet weak var division: UIButton!
     @IBOutlet weak var same: UIButton!
     @IBOutlet weak var resetButton: UIButton!
-    
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         numberLabel.text = "0"
-       
-        
+
+        numberLabel.adjustsFontSizeToFitWidth = true
+
         [number0, number1, number2, number3, number4, number5, number6, number7, number8, number9].forEach { $0.layer.cornerRadius = 41 }
         [plus, minus, multi, division, same, resetButton].forEach { $0?.layer.cornerRadius = 41 }
-        
     }
     
     var userIsInTheMiddleOfTyping: Bool = false
@@ -53,107 +42,122 @@ final class ViewController: UIViewController {
     var isEqual = false
     var isEqualxEqual = false
     
-    var sum = Int()
-    var num = Int()
-    var first: Int = 0
-    var equalNumber: Int = 0
+    var sum: Double = 0
+    var num: Double = 0
+    var first: Double = 0
+    var equalNumber: Double = 0        // 2 + = 4 이거 해결
+    var operationNumber: Double = 0         // 12 + 3 - 이거 해결 위해서
+    var arith = ""                  // 음수를 만들려고 만든 변수
+    var numbering: Double = 0
     
-//    MARK:-Button Click
+    //    MARK:-Button Click
     
     @IBAction func click(_ sender: UIButton) {
         
         let digit = sender.currentTitle!
         let inValue = numberLabel.text
-        
-        if isEqual == true {
-            numberLabel.text = digit
-        }
-        if(userIsInTheMiddleOfTyping){
+        operationNumber = num
+        if inValue!.count > 12 {
             let textCurrentlyInDisplay = numberLabel.text!
-            numberLabel.text = textCurrentlyInDisplay + digit
-//            print(userIsInTheMiddleOfTyping)
-        }else{
-            
-            if inValue != "0" {
-//                print(userIsInTheMiddleOfTyping)
-                num = Int(numberLabel.text!)!
-                if isPlus == true {
-                    if first != 0 {
-//                        print(digit)
-                        first = first + Int(digit)!
-                        numberLabel.text = digit
-//                        print(first)
-                    } else {
-//                        print(digit)
-//                        print(num)
-                        first = num + Int(digit)!
-                        numberLabel.text = digit
-                        print("여기")
-                    }
-//                    isPlus = false
-                    print("+")
-                    print("sum",sum)
-                    print("num",num)
-                }
-                if isMinus == true {
-                    if first != 0 {
-                        first = first - Int(digit)!
-                        numberLabel.text = digit
-//                        print(first)
-                    } else {
-                        first = num - Int(digit)!
-                        numberLabel.text = digit
-//                        print(first)
-                    }
-//                    isMinus = false
-                    print("-")
-                }
-                if isMulti == true {
-//                    print(first)
-                    if first != 0 {
-                        print("__________")
-                        print(first)
-                        print(digit)
-                        first = first * Int(digit)!
-                        numberLabel.text = digit
-                        print("__________")
-                        print(first)
-                        print(digit)
-//                        print(first)
-                    } else {
-                        first = num * Int(digit)!
-                        numberLabel.text = digit
-                        print("__________")
-                        print(first)
-                    }
-//                    isMulti = false
-                    print("곱")
-                }
-                if isDivi == true {
-                    if first != 0 {
-                        first = first / Int(digit)!
-                        numberLabel.text = digit
-//                        print(first)
-                    } else {
-                        first = num / Int(digit)!
-                        numberLabel.text = digit
-//                        print(first)
-                    }
-//                    isDivi = false
-                    print("sum",sum)
-                    print("num",num)
-                    print("나누")
-                }
-                
-            } else {
+            numberLabel.text = textCurrentlyInDisplay
+        } else {
+            if isEqual == true {
                 numberLabel.text = digit
-//                print(userIsInTheMiddleOfTyping)
+            }
+            if(userIsInTheMiddleOfTyping){
+                let textCurrentlyInDisplay = numberLabel.text!
+                numberLabel.text = textCurrentlyInDisplay + digit
+                //            print(userIsInTheMiddleOfTyping)
+            }else{
+                if inValue != "0" {
+                    //                print(userIsInTheMiddleOfTyping)
+                    num = Double(numberLabel.text!)!
+                    if isPlus == true {
+                        if first != 0 {
+                            //                        print(digit)
+                            first = first + Double(digit)!
+                            numberLabel.text = digit
+                            //                        print(first)
+                        } else {
+                            //                        print(digit)
+                            //                        print(num)
+                            first = num + Double(digit)!
+                            numberLabel.text = digit
+                            print("여기")
+                        }
+                        //                    isPlus = false
+                        print("+")
+                        print("sum",sum)
+                        print("num",num)
+                    }
+                    if isMinus == true {
+                        if first != 0 {
+                            first = first - Double(digit)!
+                            numberLabel.text = digit
+                            //                        print(first)
+                        } else {
+                            first = num - Double(digit)!
+                            numberLabel.text = digit
+                            //                        print(first)
+                        }
+                        //                    isMinus = false
+                        print("-")
+                    }
+                    if isMulti == true {
+                        //                    print(first)
+                        if first != 0 {
+                            print("__________")
+                            print(first)
+                            print(digit)
+                            first = first * Double(digit)!
+                            numberLabel.text = digit
+                            print("__________")
+                            print(first)
+                            print(digit)
+                            //                        print(first)
+                        } else {
+                            first = num * Double(digit)!
+                            numberLabel.text = digit
+                            print("__________")
+                            print(first)
+                        }
+                        //                    isMulti = false
+                        print("곱")
+                    }
+                    if isDivi == true {
+                        if first != 0 {
+                            first = first / Double(digit)!
+                            numberLabel.text = digit
+                            //                        print(first)
+                        } else {
+                            first = num / Double(digit)!
+                            numberLabel.text = digit
+                            //                        print(first)
+                        }
+                        //                    isDivi = false
+                        print("sum",sum)
+                        print("num",num)
+                        print("나누")
+                    }
+                    
+                } else {
+                    if arith == "-" {
+                        numberLabel.text = arith + digit
+                    } else {
+                        numberLabel.text = digit
+                    }
+                }
             }
         }
+        if inValue!.count == 13, userIsInTheMiddleOfTyping == false {
+            numberLabel.text = digit
+        }
+        arith = ""
+//        print("operationNumber", operationNumber)
         userIsInTheMiddleOfTyping = true
         isEqual = false
     }
-//    MARK:-AC
+    //    MARK:-AC
     @IBAction func AC(_ sender: UIButton) {
         
         userIsInTheMiddleOfTyping = false
@@ -167,100 +171,112 @@ final class ViewController: UIViewController {
         isPlus = false
         
         numberLabel.text = "0"
-        num = Int()
-        sum = Int()
+        num = Double()
+        sum = Double()
         first = 0
         equalNumber = 0
     }
-//    MARK:-OPERATION
+    //    MARK:-OPERATION
     @IBAction func operation(_ sender: UIButton) {
-        if isEqual == false, isDivi == true || isMinus == true || isPlus == true || isMulti == true {
-            print("얏호")
+        if userIsInTheMiddleOfTyping == false, sender.currentTitle == "-" {
+            arith = String(sender.currentTitle!)
+            numberLabel.text = arith
         }
-        
-        if isEqual == true {
+        if userIsInTheMiddleOfTyping == true, isEqual == true, arith != "-" {
             if let labelNumber = numberLabel.text {
                 if sender.currentTitle == "+", isPlus == false {
-                    num = Int(labelNumber)!
+                    num = Double(labelNumber)!
                     sum = num
                     isPlus = true; isDivi = false; isMulti = false; isMinus = false
                 } else if sender.currentTitle == "-", isMinus == false {
-                    num = Int(labelNumber)!
+                    num = Double(labelNumber)!
                     sum = num
                     isMinus = true; isDivi = false; isPlus = false; isMulti = false
+                    print("_")
                 } else if sender.currentTitle == "×", isMulti == false {
-                    num = Int(labelNumber)!
+                    num = Double(labelNumber)!
                     sum = num
                     isMulti = true; isDivi = false; isMinus = false; isPlus = false
-                    print("_______________")
                 } else if sender.currentTitle == "÷", isDivi == false {
                     if numberLabel.text == "0" {
-                     return
+                        return
                     } else {
-                    num = Int(labelNumber)!
-                    sum = num
-                    isDivi = true
+                        num = Double(labelNumber)!
+                        sum = num
+                        isDivi = true
                     }
                     isPlus = false; isMulti = false; isMinus = false
                 }
                 isEqualxEqual = true
             }
-        } else {
+            numberLabel.text = String(sum)
+        } else if userIsInTheMiddleOfTyping == true, isEqual == false, arith != "-" {
+            print("여기얏")
             if sender.currentTitle == "+", isPlus == false {
-                num = Int(numberLabel.text!)!
+                num = Double(numberLabel.text!)!
                 sum = num
                 isPlus = true; isDivi = false; isMulti = false; isMinus = false
             } else if sender.currentTitle == "-", isMinus == false {
-                num = Int(numberLabel.text!)!
+                num = Double(numberLabel.text!)!
                 sum = num
                 isMinus = true; isDivi = false; isPlus = false; isMulti = false
+                print("___")
             } else if sender.currentTitle == "×", isMulti == false {
-                num = Int(numberLabel.text!)!
+                num = Double(numberLabel.text!)!
                 sum = num
                 isMulti = true; isDivi = false; isMinus = false; isPlus = false
-                print("_______________")
             } else if sender.currentTitle == "÷", isDivi == false {
                 if numberLabel.text == "0" {
-                 return
+                    return
                 } else {
-                num = Int(numberLabel.text!)!
-                sum = num
-                isDivi = true
+                    num = Double(numberLabel.text!)!
+                    sum = num
+                    isDivi = true
                 }
                 isPlus = false; isMulti = false; isMinus = false
-            } else {
-                
             }
         }
-        numberLabel.text = String(sum)
+        print("sum",sum)
+        print("num",num)
+        print("equalNumber", equalNumber)
+        print("first", first)
+        if operationNumber != 0 {
+            equalNumber = first
+            numberLabel.text = String(equalNumber)
+            print("이곳")
+        } else {
+            numberLabel.text = String(sum)
+        }
+        print("equalNumber", equalNumber)
+        print("arith", arith)
+        print("sum",sum)
         userIsInTheMiddleOfTyping = false
     }
-//    MARK:-EQUAL
+    //    MARK:-EQUAL
     @IBAction func equal(_ sender: Any) {
         if isPlus {
             if first != 0, isEqualxEqual != true {
                 equalNumber = first
                 numberLabel.text = String(equalNumber)
                 isPlus = false
-                print("이곳")
             } else if isEqualxEqual == true {
                 equalNumber = sum + num
                 numberLabel.text = String(equalNumber)
                 isPlus = false
             } else {
-//                print(sum)
-                sum = Int(numberLabel.text!)!
-//                print(sum)
+                //                print(sum)
+                sum = Double(numberLabel.text!)!
+                //                print(sum)
                 equalNumber = num + sum
                 numberLabel.text = String(equalNumber)
-//                print(equalNumber)
+                //                print(equalNumber)
                 isPlus = false
             }
         }
-//        if isPlus == false {
-//            numberLabel.text = String(first)
-//            isPlus = false
-//        }
+        //        if isPlus == false {
+        //            numberLabel.text = String(first)
+        //            isPlus = false
+        //        }
         if isMinus {
             if first != 0, isEqualxEqual != true {
                 equalNumber = first
@@ -272,7 +288,7 @@ final class ViewController: UIViewController {
                 isMinus = false
             } else {
                 print(sum)
-                sum = Int(numberLabel.text!)!
+                sum = Double(numberLabel.text!)!
                 print(sum)
                 equalNumber = num - sum
                 numberLabel.text = String(equalNumber)
@@ -291,16 +307,17 @@ final class ViewController: UIViewController {
                 isMulti = false
             } else {
                 print(sum)
-                sum = Int(numberLabel.text!)!
+                sum = Double(numberLabel.text!)!
                 print(sum)
+                print(num)
                 equalNumber = num * sum
                 numberLabel.text = String(equalNumber)
                 print(equalNumber)
                 isMulti = false
             }
-//            sum = sum * Int(numberLabel.text!)!
-//            numberLabel.text = String(first)
-//            isMulti = false
+            //            sum = sum * Int(numberLabel.text!)!
+            //            numberLabel.text = String(first)
+            //            isMulti = false
         }
         if isDivi {
             if first != 0, isEqualxEqual != true {
@@ -315,7 +332,7 @@ final class ViewController: UIViewController {
                 isDivi = false
             } else {
                 print(sum)
-                sum = Int(numberLabel.text!)!
+                sum = Double(numberLabel.text!)!
                 print(sum)
                 equalNumber = num * sum
                 numberLabel.text = String(equalNumber)
@@ -323,7 +340,7 @@ final class ViewController: UIViewController {
                 isDivi = false
             }
         }
-        equalNumber = Int(numberLabel.text!)!
+        equalNumber = Double(numberLabel.text!)!
         numberLabel.text = String(equalNumber)
         sum = 0
         first = 0
@@ -333,5 +350,17 @@ final class ViewController: UIViewController {
         isEqual = true
     }
     
-    
 }
+
+//extension String {
+//    var inComma {
+//
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .decimal    // 천 단위로 콤마(,)
+//        formatter.minimumFractionDigits = 0    // 최소 소수점 단위
+//        formatter.maximumFractionDigits = 3    // 최대 소수점 단위
+////        let numString = formatter.string(from:  as NSNumber)
+////        print(numString)
+//    }
+//
+//}
